939565de2da183eb9b37ce9de46f2d02cfe45b14
Juaco <167112346+mjuaco@users.noreply.github.com>
2026-06-14 22:32:01 -0400

    His

--- UI/UI_Scripts/baseButton.gd
+++ UI/UI_Scripts/baseButton.gd
@@ -1,9 +1,22 @@
 extends Control
-var button = Button
+
 @onready var start_button: Button = $MarginContainer/VBoxContainer/start_button
 @onready var settings_button: Button = $MarginContainer/VBoxContainer/settings_button
 @onready var quit_button: Button = $MarginContainer/VBoxContainer/quit_button
+@onready var setting_conteiner_mg_2: MarginContainer = $MainPanel/SettingConteinerMG2
+@onready var main_conteiner_mg: MarginContainer = $MainPanel/MainConteinerMG
+@onready var back_button: Button = $MainPanel/SettingConteinerMG2/Back_button
 
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://mundo.tscn")
+
+
+func _on_settings_button_pressed() -> void:
+	main_conteiner_mg.hide()
+	setting_conteiner_mg_2.show()
+
+
+func _on_back_button_pressed() -> void:
+	main_conteiner_mg.show()
+	setting_conteiner_mg_2.hide()
