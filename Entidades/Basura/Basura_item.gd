c0eba0a55dad022831d20ef24457228ef62a0f43
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-06-25 20:11:20 -0400

    Basura añadida
    Despues de ponerme a meditarlo, basura y materiales deben ser diferentes, ya que uno es un rigidbody y el otro un area.
    
    Hice unos cuantos cambios en cuanto al tema de los nombres, de unos archivos, nada alucinante.
    
    Añadi nombres a los layers de las fisicas.

---
+++ Entidades/Basura/Basura_item.gd
@@ -0,0 +1,69 @@
+@icon("res://addons/iconos/basura.svg")
+class_name Basura extends RigidBody2D
+
+@onready var sprite2d = %Sprite
+@onready var collision_shape_2d = %CollisionShape2D
+@onready var sonido = %sonido
+
+@export var data: LootDefinicion
+
+var objetivo = null
+var input_recibido: bool = false
+var velocidad: int = -1
+
+var id: String
+var nombre: String
+enum TipoLoot {BASICO, PESADO, PAQUETE}
+var tipo_de_loot: TipoLoot
+var valor: int
+var tags: String
+
+
+func _ready():
+	iniciar_valores()
+	SignalBus.recoger_basura_automatica.connect(recibir_input)
+
+
+#func _process(delta):
+	#if input_recibido:
+		#perseguir_jugador(delta)
+
+
+func recibir_input():
+	input_recibido = true
+	print("Input recibido")
+
+
+#func perseguir_jugador(delta):
+	#if objetivo != null:
+		#global_position = global_position.move_toward(objetivo.global_position, velocidad)
+		#velocidad += 2 * delta
+
+
+func collect():
+	sonido.play()
+	collision_shape_2d.call_deferred("set", "disabled", true)
+	sprite2d.visible = false
+	return data
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Radar_Loot"):
+		objetivo = area
+
+
+func _on_sonido_finished():
+	queue_free()
+
+
+func iniciar_valores():
+
+	id = data.id
+	nombre = data.nombre
+
+	tipo_de_loot = int(data.tipo_de_loot)
+	#print(TipoLoot)
+	valor = data.valor
+	tags = data.tags
+	sonido.stream = data.audio
+	sprite2d.texture = data.sprite
