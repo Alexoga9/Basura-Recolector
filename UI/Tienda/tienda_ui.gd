65617c28e906164d507d698720a90c0e747a7dd3
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-07-02 17:01:32 -0400

    Tiendita
    Ajustes sencillos
    UN chingo de errores aparecen we, a webo hay que arreglar esa wea
    
    Toy poniendo la tienda, falta arreglar el dinero para que funcione bien y tambien la tienda esta mal implementada, no toma los valores invidualmente ni se actualizan

--- UI/Tienda/tienda_ui.gd
+++ UI/Tienda/tienda_ui.gd
@@ -7,20 +7,36 @@ extends Control
 @export var precio_recuperacion: int
 @export var precio_rango: int
 
+@onready var label_resistencia_de_energía = %"Label Resistencia de Energía"
+@onready var label_velocidad_de_recolección = %"Label Velocidad de recolección"
+@onready var label_poder_de_lanzamiento = %"Label Poder de lanzamiento"
+@onready var label_poder_de_golpe = %"Label Poder de golpe"
+@onready var label_recuperación_de_energía = %"Label Recuperación de Energía"
+@onready var label_rango_de_recogida = %"Label Rango de recogida"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	settear_precios()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func settear_precios():
+	label_resistencia_de_energía.text = str(precio_resistencia)
+	label_velocidad_de_recolección.text = str(precio_velocidadRecogida)
+	label_poder_de_lanzamiento.text = str(precio_lanzamiento)
+	label_poder_de_golpe.text = str(precio_poderGolpe)
+	label_recuperación_de_energía.text = str(precio_recuperacion)
+	label_rango_de_recogida.text = str(precio_rango)
 
 
 func verificar_precio(precio:int):
+	Inventario.modify_wallet_value(precio)
+	precio *= 1.4
+	settear_precios()
 	if Dinero.dinero >= precio:
-		pass
+		Inventario.modify_wallet_value(precio)
+		precio *= 1.4
+		settear_precios()
 
 
 func _on_resistencia_de_energía_pressed():
