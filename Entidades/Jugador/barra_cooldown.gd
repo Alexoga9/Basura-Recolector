48d1b32250fb54ff5ecb3948b48ab59a576c765f
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-07-19 09:49:53 -0400

    Barra Cooldown
    Movido de lugar de la UI al jugador mismo

---
+++ Entidades/Jugador/barra_cooldown.gd
@@ -0,0 +1,15 @@
+extends BarraDeProgreso
+
+@onready var recoge_basura = %"Recoge BASURA"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	valor_max = recoge_basura.timer.wait_time
+	valor_actual = recoge_basura.timer.time_left
--- UI/Barras/Barras HUD.gd
+++ UI/Barras/Barras HUD.gd
@@ -3,12 +3,9 @@ extends Node
 @onready var barra_energia = %BarraEnergia
 @onready var barra_basura = %BarraBasura
 
-@onready var barra_cooldown = %BarraCooldown
-
 var jugador: Jugador
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().process_frame
 	jugador = Global.jugador
@@ -23,9 +20,6 @@ func _process(delta):
 func igualar_barra_energia():
 	barra_energia.valor_max = jugador.energia_componente.energia_maxima
 	barra_energia.valor_actual = jugador.energia_componente.energia
-
-	barra_cooldown.valor_max = jugador.recoge_basura.timer.wait_time
-	barra_cooldown.valor_actual = jugador.recoge_basura.timer.time_left
 
 
 func igualar_barra_basura():
