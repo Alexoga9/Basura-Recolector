0843b42c79f39b109c7c92d8384280f1b7ddfd31
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-07-27 21:33:05 -0400

    Otoño tileset

---
+++ Entidades/Obstaculo/Obstaculo_item.gd
@@ -0,0 +1,72 @@
+@icon("res://addons/iconos/basura.svg")
+class_name Obstaculo extends StaticBody2D
+
+@onready var sprite2d = %Sprite
+@onready var collision_shape_2d = %CollisionShape2D
+@onready var sonido = %sonido
+@onready var resaltado_componente: ResaltadoComponente = %ResaltadoComponente
+@onready var t_recogida: TRecogida = %TRecogida
+
+# 📌 EL COMPONENTE DE APILAMIENTO
+@onready var componente_apilable: ComponenteApilable = %ComponenteApilable
+
+@export var data: LootDefinicion
+
+var objetivo = null
+var en_area_jugador: bool = false
+var input_recibido: bool = false
+
+var id: String
+var nombre: String
+enum TipoBasura {BASICO, PESADO, PAQUETE}
+var tipo_de_basura: TipoBasura
+var valor: int
+
+
+func _ready():
+	iniciar_valores()
+	SignalBus.recoger_basura_automatica.connect(recibir_input)
+
+
+func _process(delta):
+	# 📌 El componente gestiona la caída automáticamente.
+	# No necesitamos hacer nada aquí, solo asegurarnos de que el componente existe.
+	pass
+
+
+func recibir_input():
+	input_recibido = true
+
+
+func collect():
+	t_recogida.tween()
+	sonido.play()
+	collision_shape_2d.call_deferred("set", "disabled", true)
+
+	Inventario.add_item(data)
+	SignalBus.basura_recogida.emit()
+	return data
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Radar_Loot"):
+		objetivo = area
+
+
+func _on_sonido_finished():
+	queue_free()
+
+
+func iniciar_valores():
+	id = data.id
+	nombre = data.nombre
+	tipo_de_basura = int(data.tipo_de_basura)
+	valor = data.valor
+	sonido.stream = data.audio
+	sprite2d.texture = data.sprite
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			Global.jugador.recoge_basura.click_en_basura(self)
