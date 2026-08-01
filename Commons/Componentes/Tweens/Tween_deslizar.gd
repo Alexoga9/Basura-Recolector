21f6f6ca4a71865db95fe817a430a953fac127e8
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-08-01 16:08:53 -0400

    proceso triangulo semicirculo

--- Commons/Componentes/Tweens/Tween_deslizar.gd
+++ Commons/Componentes/Tweens/Tween_deslizar.gd
@@ -13,3 +13,14 @@ func tween():
 	tween.tween_property(padre, "position", (padre.global_position + Vector2(randi_range(-20,20), 50)), 1)\
 	.set_ease(Tween.EASE_OUT)\
 	.set_trans(Tween.TRANS_EXPO)
+
+func triangulo_compuesto():
+	var lado: float = 10
+	var base_triangulo: float = 30
+	var radio: float = base_triangulo/2
+	
+	var punto_A = Vector2(radio, lado)
+	var punto_B = Vector2(-radio, lado)
+	var punto_C = Vector2(0, 0)
+	
+	var semicirculo = (PI*pow((radio), 2)) / 2
