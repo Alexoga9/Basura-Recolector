12063946c31b35a1b7064fb53c5e7534d5f26f12
Alexoga9 <152038955+Alexoga9@users.noreply.github.com>
2026-08-29 10:51:53 -0400

    Forma basica del puesto roto
    Puestos rotos añadidos
    Usando recursos, se pueden añadir los requisitos y cuantos de estos necesita
    Se puede usar cualquier edificio que se quiera sustituir.
    Resta perfectamente los recursos usados del inventario

--- Entidades/Estaciones/Estación Rota/estación_rota.gd
+++ Entidades/Estaciones/Estación Rota/estación_rota.gd
@@ -1,57 +1,57 @@
 extends StaticBody2D
 
-# Enum
-# Array de materiales y cantidades necesarias
 @export var estacion: PackedScene
 @export var materiales: Array[MaterialesNecesarios]
 var jugador_en_area: bool
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalBus.interaccion.connect(interaccion)
 
 
-#func process():
-	#
-
-
+## Al precionar E, recibe la señal
 func interaccion():
 	if jugador_en_area:
-		matchear_enum(0)
+		for i in materiales.size():
+			matchear_enum(i)
+
 		comprobar_materiales_restantes()
 
 
+## Intenta emparejarse con el material especifico 
 func matchear_enum(posicion_array: int):
 	var material: MaterialesNecesarios
 	match materiales[posicion_array].tipo_de_elemento:
 		material.Tipo_de_Material.MADERA:
-			comprar_reparacion("Madera", posicion_array)
+			comprar_reparacion("madera", posicion_array)
 
 		material.Tipo_de_Material.PIEDRA:
-			comprar_reparacion("Piedra", posicion_array)
+			comprar_reparacion("piedra", posicion_array)
 
 		material.Tipo_de_Material.ORO:
-			comprar_reparacion("Oro", posicion_array)
+			comprar_reparacion("oro", posicion_array)
 
 		material.Tipo_de_Material.HIERRO:
-			comprar_reparacion("Hierro", posicion_array)
+			comprar_reparacion("hierro", posicion_array)
 
 
+## Busca reducir a 0 los requisitos necesarios para la reparacion
 func comprar_reparacion(ID:String, posicion_array:int):
 	print("Comprando... " + ID)
 	var costo_materiales = materiales[posicion_array].cantidad_necesaria
+	print("Tienes en el inventario " + str(Inventario.get_count(ID)) + " de " + ID)
 
-	if Inventario.has_item(ID):
-		if Inventario.get_count(ID) >= costo_materiales:
-			Inventario.remove_item(ID, costo_materiales)
-			print("Compra exitosa de " + ID)
-		else:
-			print("Necesitas "+ str(costo_materiales) + " de " +ID)
+	if Inventario.get_count(ID) >= costo_materiales and costo_materiales != 0:
+		Inventario.remove_item(ID, costo_materiales)
+		materiales[posicion_array].cantidad_necesaria = 0
+		print("Compra exitosa de " + ID)
+	elif costo_materiales == 0:
+		print("ya compraste " + ID)
 	else:
-		print("no hay ni verga de " + ID)
+		print("Necesitas "+ str(costo_materiales) + " de " +ID)
 
 
+## revisa si se cumplen las condiciones para reparar
 func comprobar_materiales_restantes():
 	var comprados: int
 	for i in materiales.size():
@@ -67,6 +67,7 @@ func comprobar_materiales_restantes():
 		print("We, te faltan vainas :v, te falta " + str(comprados))
 
 
+## Cambia la instancia de rota por una funcional
 func sustituir_por_estación():
 	var nueva_instancia = estacion.instantiate()
 	var padre = get_tree().get_first_node_in_group("Estaciones")
