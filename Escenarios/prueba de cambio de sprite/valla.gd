576e5837e9e056b9bcd08c944d32b949c3c48167
Juaco <167112346+mjuaco@users.noreply.github.com>
2026-09-01 22:11:56 -0400

    shining

--- Escenarios/prueba de cambio de sprite/valla.gd
+++ Escenarios/prueba de cambio de sprite/valla.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+@onready var vallatext: Label = %vallatext
 @onready var area_deteccion = %"Trigger Jugador"
 @onready var APuerta: AnimatedSprite2D = %puerta
 @export var porcentaje_requerido: int = 80
@@ -13,6 +14,8 @@ func _ready() -> void:
 	# Escuchamos el progreso y el botón de interacción
 	SignalBus.zona_limpida.connect(_actualizar_progreso)
 	SignalBus.interaccion.connect(_al_interactuar)
+
+	vallatext.hide()
 
 
 func _on_trigger_jugador_body_entered(body: Node2D) -> void:
@@ -31,11 +34,11 @@ func _al_acercarse(body: Node2D) -> void:
 func _al_alejarse(body: Node2D) -> void:
 	if body.is_in_group("Jugador"):
 		jugador_cerca = false
+		vallatext.hide()
 
 
 # 
 func _actualizar_progreso(nuevo_porcentaje: int) -> void:
-
 	porcentaje_actual = nuevo_porcentaje
 
 
@@ -45,6 +48,9 @@ func _al_interactuar() -> void:
 
 	if porcentaje_actual >= porcentaje_requerido:
 		abrir_paso()
+	else:
+		vallatext.show()
+		vallatext.text = "Tienes que limpiar el " + str(porcentaje_requerido) +" % "
 
 
 func abrir_paso() -> void:
